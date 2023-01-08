6834f7506dd19befa9fdde7d7d3d151df9281313
Amani Mavu <amanimavu@gmail.com>
2023-01-08 18:05:13 +0300

    all task on more classes and objects

---
+++ 0-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+# 0-rectangle module
+"""
+This module contains a class Rectangle
+
+>>> Rectangle = __import__('0-rectangle').Rectangle
+
+>>> my_rectangle = Rectangle()
+>>> print(type(my_rectangle))
+<class '0-rectangle.Rectangle'>
+
+>>> print(my_rectangle.__dict__)
+{}
+
+"""
+
+class Rectangle:
+    """This is an empty class"""
+    pass
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
